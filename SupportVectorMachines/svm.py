ebf6c9132f0f925003d35973a63c78f10b768194
24online24 <chiuzbaianrares20@gmail.com>
2022-10-20 23:08:03 +0300

    All finished I reckon

--- SupportVectorMachines/svm.py
+++ SupportVectorMachines/svm.py
@@ -6,12 +6,23 @@ from sklearn.neighbors import KNeighborsClassifier
 
 dataset = datasets.load_breast_cancer()
 
-print(dataset.feature_names)
-print(dataset.target_names)
+# print(dataset.feature_names)
+# print(dataset.target_names)
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
     dataset.data, dataset.target, test_size=0.2)
 
-print(x_train)
-print(y_train)
+# print(x_train)
+# print(y_train)
 classes = ['malignant', 'benign']
+
+clf = svm.SVC(kernel='linear', C=2)
+
+# clf = KNeighborsClassifier(n_neighbors=9)
+clf.fit(x_train, y_train)
+
+y_pred = clf.predict(x_test)
+
+acc = metrics.accuracy_score(y_test, y_pred)
+
+print(acc)
